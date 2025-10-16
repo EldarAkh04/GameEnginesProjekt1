3ae2aa49d3c96ad039ab84c4c645167b7be6497e
EldarAkh04 <114343028+EldarAkh04@users.noreply.github.com>
2025-10-16 20:40:43 +0200

    Playermovement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private bool jumped;
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpSpeed;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D> ();
+    }
+
+    private void Update(){
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed,body.velocity.y);
+
+        
+
+        if(Input.GetKey(KeyCode.Space) && jumped == true){
+            jumped = false;
+            body.velocity = new Vector2(body.velocity.x, jumpSpeed);
+        }
+    }
+
+    private bool OnCollisionEnter2D(Collision2D collision){
+        Debug.Log(collision.gameObject.name);
+        jumped = true;
+        return jumped;
+    }
+
+} 
