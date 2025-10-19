77ff5edafea7183cba7662894db483f40a01331f
EldarAkh04 <114343028+EldarAkh04@users.noreply.github.com>
2025-10-19 13:06:12 +0200

    Disappearance of the ball
    If the ball hits Square1 or Square 2 it disappears

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,9 +7,29 @@ public class Bullet : MonoBehaviour
     
     public float bulletSpeed = 20f;
     public Rigidbody2D rb;
+    
 
     void Start()
     {
           rb.velocity = transform.right * bulletSpeed;
+    }
+
+    void Awake() 
+    { 
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D collide)
+    {
+        var ShootingBullet = collide.gameObject.name;
+        Debug.Log(ShootingBullet);
+
+
+        if(ShootingBullet == "Square1" || ShootingBullet == "Square2")
+        {
+            Destroy(gameObject);
+        }
     }
 }
